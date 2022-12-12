606f35c27ab9b950a0f3ec9ac6c8e5d1592d9a6e
Anna Karutina <anna.karutina@voco.ee>
2022-12-12 11:06:03 +0200

    Create delete single task functionality

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const form = document.querySelector('form')
 form.addEventListener('submit', addTask)
 
+const ul = document.querySelector('ul')
+ul.addEventListener('click', deleteTask)
+
+function deleteTask(event){
+    if(event.target.textContent === 'X'){
+        if(confirm('Are you sure to delete this task?')){
+            event.target.parentElement.remove()
+        }
+    }
+}
+
+
 function addTask(event) {
     const taskText = document.querySelector('#task').value
 
@@ -17,7 +29,6 @@ function addTask(event) {
 
     li.appendChild(a)
 
-    const ul = document.querySelector('ul')
     ul.appendChild(li)
 
     document.querySelector('#task').value = ''
